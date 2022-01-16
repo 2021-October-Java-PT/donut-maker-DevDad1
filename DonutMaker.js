5a7eec9e388e85486208a72c4403f85f470f7e1a
DevDad1 <ryan@ryanwebster.dev>
2022-01-15 20:34:11 -0500

    added feature 2

--- DonutMaker.js
+++ DonutMaker.js
@@ -4,7 +4,6 @@
 // // Get the button that opens the modal
 // var btn = document.getElementById("modalBtn");
 
-
 // // Get the <span> element that closes the modal
 // var span = document.getElementsByClassName("close")[0];
 
@@ -25,49 +24,55 @@
 //   }
 // }
 
-
 class Donut {
-    
-  constructor(donutCount, autoClickerCount, clickMultiplierCount) {
-      this.donutCount = donutCount;
-      this.autoClickerCount = autoClickerCount;
-      this.clickMultiplierCount = clickMultiplierCount;
-  }
   
+  constructor(
+    donutCount,
+    autoClickerCount,
+    clickMultiplierCount,
+    clickMultiplierVar
+  ) {
+    this.donutCount = donutCount;
+    this.autoClickerCount = autoClickerCount;
+    this.clickMultiplierCount = clickMultiplierCount;
+    this.clickMultiplierVar = clickMultiplierVar;
+  }
+
   click() {
-      this.donutCount += 1; 
+    this.donutCount += 1;
   }
-  
-  
-    
-  
+
   purchaseAutoClicker() {
-   if (this.donutCount >= 100){    
-     if (this.autoClickerCount === 0 && this.donutCount >= 100) { 
-    this.donutCount -=100;
-    } else if(this.autoClickerCount === 1 && this.donutCount >= 110){
-     this.donutCount -=110;
-    } else if(this.autoClickerCount === 2 && this.donutCount >= 121){
-      this.donutCount -= 121;
-    } else if(this.autoClickerCount >= 3 && this.donutCount >=133){
-      this.donutCount -= 133;
+    if (this.donutCount >= 100) {
+      if (this.autoClickerCount === 0 && this.donutCount >= 100) {
+        this.donutCount -= 100;
+      } else if (this.autoClickerCount === 1 && this.donutCount >= 110) {
+        this.donutCount -= 110;
+      } else if (this.autoClickerCount === 2 && this.donutCount >= 121) {
+        this.donutCount -= 121;
+      } else if (this.autoClickerCount >= 3 && this.donutCount >= 133) {
+        this.donutCount -= 133;
+      }
+      this.autoClickerCount += 1;
     }
-    this.autoClickerCount +=1;
-   }
-} 
-
-enableAutoClickers() {
-  this.donutCount += (this.autoClickerCount) * 1;
-}
-
-purchaseClickMultiplier() {
-  this.clickMultiplierCount += 1;
-  this.donutCount -= 10;
-  
-}
-  
-      
   }
-  
-export default Donut;
 
+  enableAutoClickers() {
+    this.donutCount += this.autoClickerCount * 1;
+  }
+
+  purchaseClickMultiplier() {
+    if (this.clickMultiplierCount === 0) {
+      this.clickMultiplierCount += 1;
+      this.donutCount -= 10;
+      this.clickMultiplierVar = 10;
+    } else {
+      this.clickMultiplierVar = this.clickMultiplierVar * 1.1;
+      this.clickMultiplierCount += 1;
+      this.donutCount -= this.clickMultiplierVar;
+      
+    }
+  }
+}
+
+export default Donut;
--- DonutMaker.test.js
+++ DonutMaker.test.js
@@ -70,41 +70,36 @@ describe("Donut", () => {
   });
 
   test("does it add an clickMultiplierCount", () => {
-    const underTest = new Donut(10, 0, 0);
+    const underTest = new Donut(10, 0, 0, 0);
     underTest.purchaseClickMultiplier();
     expect(underTest.clickMultiplierCount).toEqual(1);
   });
 
   test("does it remove 10 donuts, and add an clickMultiplierCount", () => {
-    const underTest = new Donut(10, 0, 0);
+    const underTest = new Donut(10, 0, 0, 0);
     underTest.purchaseClickMultiplier();
     expect(underTest.clickMultiplierCount).toEqual(1);
     expect(underTest.donutCount).toEqual(0);
   });
+
+  test("should have clickMultiplierVar set to 10", () => {
+    const underTest = new Donut(10, 0, 0, 0);
+    expect(underTest.clickMultiplierVar).toEqual(0);
+    underTest.purchaseClickMultiplier();
+    expect(underTest.clickMultiplierVar).toEqual(10);
+  });
+
+  test("a second clickMultiplier should cost 10 percent more", () => {
+    const underTest = new Donut(12, 0, 1, 10);
+    underTest.purchaseClickMultiplier();
+    expect(underTest.clickMultiplierCount).toEqual(2);
+    expect(underTest.donutCount).toEqual(1);
+  });
+
+  test("each clickMultiplier should cost 10 percent more", () => {
+    const underTest = new Donut(13, 0, 2, 11);
+    underTest.purchaseClickMultiplier();
+    expect(underTest.clickMultiplierCount).toEqual(3);
+    expect(underTest.donutCount).toEqual(0.8999999999999986);
+  });
 });
-//
-//
-//
-//
-//Feature 1
-// Can add to donut count.
-// Can retrieve a donut count.
-// FEATURE 2 : Be able to purchase the first Auto Clicker with 100 donuts from your donut count.
-// As a user, I want to be able to purchase Auto Clickers, so that I don't have to keep clicking a button all day.
-
-// Can retrieve an Auto Clicker count.
-// Can add to the Auto Clicker count.
-// Subtract the Auto Clicker cost from your donut count.
-// FEATURE 3 : The cost of each Auto Clicker will go up with each purchase.
-// As a game designer, I want the game to become more difficult as the game progresses, so that users will continue to play the game.
-
-// Increase the cost of the second Auto Clicker by an additional ten percent.
-// Increase the cost of every additional Auto Clicker by an additional ten percent.
-// FEATURE 4 : Ensure that there are enough clicks to buy a Auto Clicker.
-// As the game designer, I want to ensure that players have to put the game into a proper state to be able to purchase a Auto Clicker, so that the game has a challenge.
-
-// Prevent the Auto Clicker count from going up if there are not enough clicks to purchase a Auto Clicker.
-// FEATURE 5 : The amount of Auto Clickers affect the amount of donuts added when an 'Activate Auto Clickers' event is called.
-// As a user, I want to be able to purchase Auto Clickers, so that I don't have to keep clicking a button all day.
-
-// When the 'activate Auto Clickers' event is executed, increase the donut total by the amount of Auto Clickers owned.
