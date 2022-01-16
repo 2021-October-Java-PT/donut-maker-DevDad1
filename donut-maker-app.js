24d6ad0758987e952550689a0aa3c23f2d43ce82
DevDad1 <ryan@ryanwebster.dev>
2022-01-16 18:04:14 -0500

    iteration 3 complete

--- donut-maker-app.js
+++ donut-maker-app.js
@@ -1,27 +1,31 @@
-import Donut from "./DonutMaker";
+var btn = document.querySelectorAll("button.modal-button");
 
-// Get the modal
-var modal = document.getElementById("myModal");
+var modals = document.querySelectorAll(".modal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("modalBtn");
+var spans = document.getElementsByClassName("close");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function (e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+  };
+}
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+for (var i = 0; i < spans.length; i++) {
+  spans[i].onclick = function () {
+    for (var index in modals) {
+      if (typeof modals[index].style !== "undefined")
+        modals[index].style.display = "none";
+    }
+  };
+}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target.classList.contains("modal")) {
+    for (var index in modals) {
+      if (typeof modals[index].style !== "undefined")
+        modals[index].style.display = "none";
+    }
   }
 };
