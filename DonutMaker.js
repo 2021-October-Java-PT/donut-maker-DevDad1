92c817ba49ea99f70f68097a87a30b7f3fd221e5
DevDad1 <ryan@ryanwebster.dev>
2022-01-16 17:09:32 -0500

    features1,2,3,4

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,51 +1,21 @@
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("modalBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
 class Donut {
-  
   constructor(
     donutCount,
     autoClickerCount,
     clickMultiplierCount,
-    clickMultiplierVar) 
-    {
+    clickMultiplierVar
+  ) {
     this.donutCount = donutCount;
     this.autoClickerCount = autoClickerCount;
     this.clickMultiplierCount = clickMultiplierCount;
     this.clickMultiplierVar = clickMultiplierVar;
-    
-    
   }
-  
 
   click() {
-    if(this.clickMultiplierCount === 0){
-    this.donutCount += 1;
-    } else{
-      this.donutCount += Math.pow(1.2,this.clickMultiplierCount);
+    if (this.clickMultiplierCount === 0) {
+      this.donutCount += 1;
+    } else {
+      this.donutCount += Math.pow(1.2, this.clickMultiplierCount);
     }
   }
 
@@ -65,29 +35,29 @@ class Donut {
   }
 
   enableAutoClickers() {
-    if (this.autoClickerCount ===0){
-      console.log('no auto clickers available')
-    }else if(this.clickMultiplierCount === 0 && this.autoClickerCount >= 1){
+    if (this.autoClickerCount === 0) {
+      console.log("no auto clickers available");
+    } else if (this.clickMultiplierCount === 0 && this.autoClickerCount >= 1) {
       this.donutCount += this.autoClickerCount * 1;
-    } else{
-    this.donutCount +=  (Math.pow(1.2,this.clickMultiplierCount) * this.autoClickerCount);
+    } else {
+      this.donutCount +=
+        Math.pow(1.2, this.clickMultiplierCount) * this.autoClickerCount;
+    }
   }
-}
 
   purchaseClickMultiplier() {
     if (this.donutCount >= 10) {
-    if (this.clickMultiplierCount === 0) {
-      this.clickMultiplierCount += 1;
-      this.donutCount -= 10;
-      this.clickMultiplierVar = 10;
-    } else {
-      this.clickMultiplierVar = this.clickMultiplierVar * 1.1;
-      this.clickMultiplierCount += 1;
-      this.donutCount -= this.clickMultiplierVar;
+      if (this.clickMultiplierCount === 0) {
+        this.clickMultiplierCount += 1;
+        this.donutCount -= 10;
+        this.clickMultiplierVar = 10;
+      } else {
+        this.clickMultiplierVar = this.clickMultiplierVar * 1.1;
+        this.clickMultiplierCount += 1;
+        this.donutCount -= this.clickMultiplierVar;
+      }
     }
   }
 }
-} 
-  
 
 export default Donut;
